ea6ccaadfd60a51d14b3d4fa48a2aff205e5e7c3
Garroveta <garroveta.c@gmail.com>
2026-08-03 18:17:18 +0200

    build: add GitHub Pages target

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { sites } from './build/sites-vite-plugin'
 
-export default defineConfig(async () => {
+export default defineConfig(async ({ mode }) => {
+  const isGitHubPagesBuild = mode === 'pages'
+
+  if (isGitHubPagesBuild) {
+    return {
+      base: '/',
+      build: {
+        outDir: 'dist-pages',
+      },
+      plugins: [react()],
+    }
+  }
+
   process.env.WRANGLER_WRITE_LOGS ??= 'false'
   process.env.WRANGLER_LOG_PATH ??= '.wrangler/logs'
   process.env.MINIFLARE_REGISTRY_PATH ??= '.wrangler/registry'
